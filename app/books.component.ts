3e97fbed36e118b6ac6b313e84380f6382b4c749
Anastasia <anastasia.aizman@gmail.com>
2016-05-30 14:36:53 -0400

    add - ability to click on books

--- app/books.component.ts
+++ app/books.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl:'./app/books.component.html',
 })
-export class AppComponent {
-  books = BOOKS;
+export class BooksComponent {
+  selectedBook: Book;
+  onSelect(book:Book) { this.selectedBook = book; }
+  // exposes books for binding
+  public books = BOOKS;
 }
 
 export class Book {
@@ -15,14 +18,14 @@ export class Book {
 }
 
 var BOOKS: Book[] = [
-  { "id": 11, "title":  "Mr. Nice" },
-  { "id": 12, "title": "Narco" },
-  { "id": 13, "title": "Bombasto" },
-  { "id": 14, "title": "Celeritas" },
-  { "id": 15, "title": "Magneta" },
-  { "id": 16, "title": "RubberMan" },
-  { "id": 17, "title": "Dynama" },
-  { "id": 18, "title": "Dr IQ" },
-  { "id": 19, "title": "Magma" },
-  { "id": 20, "title": "Tornado" }
+  { "id": 11, "title": "Mr. Nice", "author": "Me" },
+  { "id": 12, "title": "Narco", "author": "Me" },
+  { "id": 13, "title": "Bombasto", "author": "Me" },
+  { "id": 14, "title": "Celeritas", "author": "Me" },
+  { "id": 15, "title": "Magneta", "author": "Me" },
+  { "id": 16, "title": "RubberMan", "author": "Me" },
+  { "id": 17, "title": "Dynama", "author": "Me" },
+  { "id": 18, "title": "Dr IQ", "author": "Me" },
+  { "id": 19, "title": "Magma", "author": "Me" },
+  { "id": 20, "title": "Tornado", "author": "Me"  }
 ];
